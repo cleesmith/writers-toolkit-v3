ea603a85b148a0020e1477d625ef0fd00f147c77
CleeSmith <cleesmith2006@gmail.com>
2025-04-23 08:30:00 -0400

    fixes

--- forge.config.js
+++ forge.config.js
@@ -3,7 +3,7 @@ const { FuseV1Options, FuseVersion } = require('@electron/fuses');
 
 module.exports = {
   packagerConfig: {
-    asar: true,
+    asar: false,
     name: 'WritersToolkit',
     executableName: 'writers-toolkit',
     icon: './resources/icon',
@@ -19,38 +19,6 @@ module.exports = {
     {
       name: '@electron-forge/maker-dmg',
       icon: './resources/WritersToolkit.icns',
-      config: {
-        background: './resources/.background/background.png',
-        format: 'ULFO'
-      }
-    }
-  ],
-  plugins: [
-    {
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
-    new FusesPlugin({
-      version: FuseVersion.V1,
-      [FuseV1Options.RunAsNode]: false,
-      [FuseV1Options.EnableCookieEncryption]: true,
-      [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-      [FuseV1Options.EnableNodeCliInspectArguments]: false,
-      [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-      [FuseV1Options.OnlyLoadAppFromAsar]: true,
-    }),
-  ],
-  // Add this if you want to publish to GitHub
-  publishers: [
-    {
-      name: '@electron-forge/publisher-github',
-      config: {
-        repository: {
-          owner: 'cleesmith',
-          name: 'writers-toolkit-v2'
-        },
-        prerelease: false
-      }
     }
   ]
 };
